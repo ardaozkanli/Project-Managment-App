58b5bb659facc35dddcb8807ce3e79f31e941c28
ardaozkanli <ardaozkanli@hotmail.com>
2024-11-01 09:05:13 +0300

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import ProjectSidebar from "./components/ProjectSidebar";
+import NewProject from "./components/NewProject";
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar />
+      <NewProject />
+    </main>
   );
 }
 
